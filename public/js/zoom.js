c1f24dd931f356fbde5b2c2d8e12b0c77ecff2d4
Kubenek <Kubenku.of@gmail.com>
2025-06-02 09:38:30 +0200

    fix zoom text to not scale and stay same size & pos at all times

--- public/js/zoom.js
+++ public/js/zoom.js
@@ -1,14 +1,10 @@
 function updateZoomTracker() {
-    const zoom = window.outerWidth / window.innerWidth;
-    const roundedZoom = zoom.toFixed(1);
-
-    const tracker = document.getElementById("zoom-tracker");
-    tracker.textContent = `${roundedZoom}x`;
-
-    tracker.style.transform = `scale(${1 / zoom})`;
+    const zoomTR = document.querySelector(".zoom-tracker");
+    const zoom = (window.outerWidth / window.innerHeight).toFixed(1)
+    zoomTR.textContent = `${zoom}x`
 }
 
 document.addEventListener("wheel", updateZoomTracker);
 window.addEventListener("resize", updateZoomTracker);
 
-updateZoomTracker(); 
+updateZoomTracker();
