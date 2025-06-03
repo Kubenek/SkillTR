8efbcea97d08cc4789a6162e5698a5c6b894200e
Kubenek <Kubenku.of@gmail.com>
2025-06-03 18:21:52 +0200

    fix: toolbar size and pos not affected by zoom level

--- public/js/zoom.js
+++ public/js/zoom.js
@@ -8,3 +8,17 @@ document.addEventListener("wheel", updateZoomTracker);
 window.addEventListener("resize", updateZoomTracker);
 
 updateZoomTracker();
+
+function lockToolbar() {
+    const nav = document.querySelector(".nav");
+
+    if (!nav) return;
+
+    const zoom = window.devicePixelRatio;
+    const invertScale = 1.5 / zoom
+
+    nav.style.transform = `translateX(-50%) scale(${invertScale})`
+}
+
+window.addEventListener('load', lockToolbar);
+window.addEventListener('resize', lockToolbar)
