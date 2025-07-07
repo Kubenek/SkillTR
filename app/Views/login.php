381f45228eb4015b9a55d723b630ccd149abb097
Kubenek <Kubenku.of@gmail.com>
2025-07-07 10:53:37 +0300

    begin restyling login page, add gradient bg

--- app/Views/login.php
+++ app/Views/login.php
@@ -10,41 +10,7 @@
 <body>
     <main class="site-container">
 
-        <section class="main-content">
-
-            <div class="left-box">
-
-                <div class="top-text">
-                    <p class="lg">Login</p>
-                    <p class="sm">Enter your credasentials to sign in</p>
-                </div>
-
-                <div class="input-fields">
-
-                    <div class="input-box">
-                        <label for="mail-input">Email</label>
-                        <input type="text" id="mail-input">
-                    </div>
-
-                    <div class="input-box">
-                        <label for="pass-input">Password</label>
-                        <input type="password" id="pass-input">
-                    </div>
-
-                    <div class="rememberBox">
-                        <input type="checkbox" id="remember">
-                        <label for="remember">Remember me</label>
-                    </div>
-
-                </div>
-
-            </div>
-
-            <div class="right-box">
-
-            </div>
-
-        </section>
+    
 
     </main>
 </body>
