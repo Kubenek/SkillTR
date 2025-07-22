038c68b2e949ab66548c9d97e5e1eeef6771e161
Kubenek <Kubenku.of@gmail.com>
2025-07-22 12:04:15 +0200

    change color on shift press of icon n icon text, only when delete is selected

--- public/js/creator/delete.js
+++ public/js/creator/delete.js
@@ -1,17 +1,29 @@
 import { creatorState } from './creatorState.js'
 
-const indicator = document.querySelector(".indicator");
-
 document.addEventListener("keydown", (e) => {
-    if(e.shiftKey) {
+
+    const indicator = document.querySelector(".indicator");
+    const linkText = (document.querySelectorAll(".link-text"))[2]
+    const linkIcon = (document.querySelectorAll(".link-icon"))[2]
+
+    if(e.shiftKey && creatorState.currentItem === "Delete") {
         creatorState.disableDrag = true;
         indicator.style.border = "0.4rem solid #FFA500";
+        linkIcon.style.color = "#FFA500";
+        linkText.style.color = "#FFA500";
     }
 })
 
 document.addEventListener("keyup", (e) => {
-    if(!e.shiftKey) {
+
+    const indicator = document.querySelector(".indicator");
+    const linkIcon = (document.querySelectorAll(".link-icon"))[2]
+    const linkText = (document.querySelectorAll(".link-text"))[2]
+
+    if(!e.shiftKey && creatorState.currentItem === "Delete") {
         creatorState.disableDrag = false
         indicator.style.border = "0.4rem solid #4070F4";
+        linkIcon.style.color = "#4070F4";
+        linkText.style.color = "#4070F4";
     }
 })
