573d052c7e4e0fecbdee724aaa2d5c6c56229ccc
Kubenek <Kubenku.of@gmail.com>
2025-07-30 11:33:41 +0200

    add functionality to pass visibility change

--- public/js/login.js
+++ public/js/login.js
@@ -1,11 +1,20 @@
 import { switchIcon } from "./QoL.js";
 
 const eyeIcon = document.getElementById("eyeIcon")
+
 eyeIcon.addEventListener("click", () => {
     let state = eyeIcon.dataset.icon
 
     if(state === "0") { switchIcon('eyeIcon', '/../images/icons/bx-eye-closed.png'); state = "1"; }
     else { switchIcon('eyeIcon', '/../images/icons/bx-eye-alt.png'); state = "0" }
 
+    togglePassVisibility(state)
+
     eyeIcon.dataset.icon = state;
 })
+
+function togglePassVisibility(state) {
+    const passInput = document.querySelector(".pass-input")
+    if(state === "0") passInput.type = "text";
+    else passInput.type = "password"
+}   
