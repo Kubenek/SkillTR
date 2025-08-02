cea6fafa8e950cd6192c4db16d508be5ea1cf963
Kubenek <Kubenku.of@gmail.com>
2025-08-02 13:22:43 +0200

    add routing logic, depending on request method either show page or turn it into login script

--- public/index.php
+++ public/index.php
@@ -3,15 +3,17 @@
 require_once __DIR__ . "/../vendor/autoload.php";
 
 $path = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
+$method = $_SERVER['REQUEST_METHOD'];
 
 $fullStaticPath = __DIR__ . $path;
 if ($path !== '/' && file_exists($fullStaticPath)) {
     return false;
 }
 
+
 switch ($path) {
     case "/login":
-        App\Controllers\AuthController::showLogin();
+        ($method === "GET") ? App\Controllers\AuthController::showLogin() : App\Controllers\AuthController::login();
         break;
     case "/creator":
         App\Controllers\CreatorController::showCreator();
