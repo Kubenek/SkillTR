2b848ee5db6616ebe1cae47bd2c9f785bea2d6e0
Kubenek <Kubenku.of@gmail.com>
2025-08-05 13:29:24 +0200

    add dotenv to composer, start connection to mysql server

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,1 +1,13 @@
+<?php
 
+require '../../vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+$dbHost = $_ENV['DB_HOST'];
+$dbUser = $_ENV['DB_USERNAME'];
+$dbPass = $_ENV['DB_PASSWORD'];
+$dbDatabase = $_ENV['DB_DATABASE'];
+
+$conn = new mysqli($dbHost, $dbUser, $dbPass, $dbDatabase);
