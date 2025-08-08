87684f667abd00efa9545f680d3ab0dc539b8b8d
Kubenek <Kubenku.of@gmail.com>
2025-08-08 10:52:20 +0200

    add variables to user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,4 +4,10 @@ require '../../vendor/autoload.php';
 
 $conn = Database::getConnection();
 
-
+class User {
+    private int $id;
+    private string $username;
+    private string $passHash;
+    private string $email;
+    private string $createdAt;
+}
