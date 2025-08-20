6fdd1acd9e287eca951beab446015e0a0f2c560b
Kubenek <Kubenku.of@gmail.com>
2025-08-20 16:05:19 +0200

    sidebar fixes: user profile fix

--- app/Views/Components/sidebar.php
+++ app/Views/Components/sidebar.php
@@ -54,10 +54,15 @@
     </ul>
     <div class="user-account">
         <div class="user-profile">
-            <img src="images/profile-img.jpg" alt="Profile Image" />
+            <svg xmlns="http://www.w3.org/2000/svg" width="42px" fill="currentColor" viewBox="0 0 24 24">
+                <path
+                    d="M12 2C6.49 2 2 6.49 2 12s4.49 10 10 10 10-4.49 10-10S17.51 2 12 2m0 5c1.73 0 3 1.27 3 3s-1.27 3-3 3-3-1.27-3-3 1.27-3 3-3m0 13a7.98 7.98 0 0 1-5.48-2.18C7.33 16.16 9.03 15 11 15h2c1.97 0 3.66 1.16 4.47 2.82A7.94 7.94 0 0 1 12 20">
+                </path>
+            </svg>
             <div class="user-detail">
-                <h3>Eva Murphy</h3>
-                <span>Web Developer</span>
+                <h3><?php $user = $_SESSION['user'];
+                echo $user->getUsername(); ?></h3>
+                <span>Personal Account</span>
             </div>
         </div>
     </div>
